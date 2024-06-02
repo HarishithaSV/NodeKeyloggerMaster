fe187ddb12a783873cc97344babb589a39ce5704
HarishithaSV <144369918+HarishithaSV@users.noreply.github.com>
2024-06-02 14:59:20 +0530

    Create keycodes.js

---
+++ keycodes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Keys = {
+  '1': 'KEY_ESC',
+  '2': 'KEY_1',
+  '3': 'KEY_2',
+  // ... (omitted for brevity)
+  '187': 'KEY_F18',
+  '188': 'KEY_F19',
+  '189': 'KEY_F20',
+  '190': 'KEY_F21',
+  '191': 'KEY_F22',
+  '192': 'KEY_F23',
+  '193': 'KEY_F24',
+  '240': 'KEY_UNKNOWN'
+}
+
+module.exports = Keys;
